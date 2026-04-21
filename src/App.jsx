0a10bff32cc0e3249c73fef5e74761a113a14f76
neetibut <neetibut@gmail.com>
2026-04-21 14:14:14 +0700

    update: react day 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Castle from "./components/Castle";
+import Castle from "./components/01_Castle";
 
 export default function App() {
   // creating state variables
@@ -13,9 +13,10 @@ export default function App() {
   return (
     <div className="pb-80 py-10 gap-y-4 flex flex-col justify-center items-center min-h-screen bg-gray-800 text-white">
       <p className="text-purple-300">
-        Message for JSD12:
+        Message for JSD12: {" "}
         <span className="text-yellow-300">
           {/* question or waiting for a message */}
+          {question ? question : "Waiting for a message..."}
         </span>
       </p>
       <textarea
@@ -30,7 +31,7 @@ export default function App() {
           {/* answer or waiting for a reply */}
         </span>
       </p>
-      <Castle />
+      <Castle banana={question} apple={""} orange={""} />
     </div>
   );
 }
